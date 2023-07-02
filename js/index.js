50546a923f441abae7b776f841e438e74780961b
Crdzcode <78108996+Crdzcode@users.noreply.github.com>
2023-07-02 16:15:51 -0300

    feat: first screens of FriendsWithMoney

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//Função apenas para ter uma navegabilidade simples, o cadastro não redirecionará para o login na realidade xD
+
+function redirect() {
+    window.location.href = "./pages/login.html";
+}
